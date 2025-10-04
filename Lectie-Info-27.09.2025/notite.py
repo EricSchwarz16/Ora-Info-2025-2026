9b47a68e18868e795d79d6f1a07a91df4124c0bf
EricSchwarz16 <ericschwarz2008@gmail.com>
2025-10-04 11:46:40 +0200

    lectie 27 septembrie

--- Lectie-Info-27.09.2025/notite.py
+++ Lectie-Info-27.09.2025/notite.py
@@ -6,7 +6,7 @@ in python compilarea se face linie cu linie => pot rula programul pana ajung la 
 """
 
 """
-in c++ ca sa ramanem in scope-ul unui for/if/while fom folosi acolada {}
+in c++ ca sa ramanem in scope-ul unui for/if/while vom folosi acolada {}
 pythonul e putin mai special deoarece nu are nevoie de acolade -> ca sa ramai in scope-ul unei
 instructiuni se foloseste indentarea
 
@@ -53,7 +53,7 @@ for(int i = 1; i <= 20; ++i) {
 }
 """
 
-for i in range(20): #in python se face automat indexarea de la 0 si merge pana la target - 1 => forul nostru merge de la 0 la 19
+for i in range(20): #in python se face automat indexarea de la 0 si merge pana la target - 1 => for-ul nostru merge de la 0 la 19
     print(i)
     
 for i in range(10, 20): # de la 10 la 19
