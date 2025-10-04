2adddafca8efb57ed94c103dd41b6903b4e3cfc4
EricSchwarz16 <ericschwarz2008@gmail.com>
2025-10-04 11:49:11 +0200

    problema_ora

---
+++ Lectie-info-03.10.2025/#847.py
@@ -0,0 +1,18 @@
+prop = input()
+#1 Separarea fiecarui cuvant
+
+lista_cuv = sorted(prop.split(" "))
+#2 Numararea fiecarui cuvant in parte asemanator vector frecv (dict)
+
+ord_cuv = {}
+for cuv in lista_cuv:
+    if cuv in ord_cuv.keys():
+        ord_cuv[cuv] += 1
+    
+    else:
+        ord_cuv[cuv] = 1
+
+     
+#3 Ordonare lexicografica si afisare cheie si valoare
+
+print(ord_cuv)
