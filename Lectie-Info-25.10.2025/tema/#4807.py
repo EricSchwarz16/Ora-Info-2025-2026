26f4d9422895dfe1494738d17fa92c885f05a12e
EricSchwarz16 <ericschwarz2008@gmail.com>
2025-10-25 23:11:23 +0200

    uno_redo_pbInfo-problem_completion

--- Lectie-Info-25.10.2025/tema/#4807.py
+++ Lectie-Info-25.10.2025/tema/#4807.py
@@ -1,4 +1,63 @@
-with open ("librarie.in", "r")as fin:
-    nr_carti = int(fin.readline())
-    preturi = list(map(int, fin.readline().split()))
-    Q = int(fin.readline())
+def procesare_preturi():
+    with open("librarie.in", "r") as f:
+        n = int(f.readline()) 
+        preturi = list(map(int, f.readline().split()))
+        Q = int(f.readline()) 
+
+        undo_stack = []
+        redo_stack = []
+
+        for zi in range(Q):
+            t = int(f.readline()) 
+            operatii = []
+
+            for op in range(t):
+                operatii.append(f.readline().strip().split())
+
+            for operatie in operatii:
+                if operatie[0] == "+" or operatie[0] == "-":
+                    tip = operatie[0] 
+                    index1 = int(operatie[1]) - 1  
+                    index2 = int(operatie[2]) - 1
+                    valoare = int(operatie[3])
+
+                    
+                    undo_stack.append(("modificare", index1, index2, tip, valoare))
+                    redo_stack.clear()  # Anulăm redo-ul dacă există operații noi
+
+                    for i in range(index1, index2 + 1):
+                        if tip == "+":
+                            preturi[i] += valoare
+                        elif tip == "-":
+                            preturi[i] -= valoare
+
+                elif operatie[0] == "u":
+                    if undo_stack:
+                        last_op = undo_stack.pop()
+                        index1, index2, tip, valoare = last_op
+
+                        redo_stack.append(last_op)
+                        for i in range(index1, index2 + 1):
+                            if tip == "+":
+                                preturi[i] -= valoare 
+
+                            elif tip == "-":
+                                preturi[i] += valoare  
+
+                elif operatie[0] == "r":
+                    if redo_stack:
+                        last_op = redo_stack.pop()
+                        index1, index2, tip, valoare = last_op
+
+                        undo_stack.append(last_op)
+                        for i in range(index1, index2 + 1):
+                            if tip == "+":
+                                preturi[i] += valoare 
+
+                            elif tip == "-":
+                                preturi[i] -= valoare  
+
+            with open("librarie.out", "a") as fout:
+                fout.write(" ".join(map(str, preturi)) + "\n")
+
+procesare_preturi()
