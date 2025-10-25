a7c54a4c61040d6637f7f3af61abd10ffe768bf9
EricSchwarz16 <ericschwarz2008@gmail.com>
2025-10-25 16:53:18 +0200

    homework completion

--- Lectie-Info-25.10.2025/tema/models.py
+++ Lectie-Info-25.10.2025/tema/models.py
@@ -1,6 +1,9 @@
 class Medicine:
-    def _init_(self, name, concentration, quantity, price):
+    def __init__(self, name, concentration, quantity, price):
         self.name = name
         self.concentration = concentration
         self.quantity = quantity
         self.price = price
+
+    def __repr__(self):
+        return f"{self.name} | {self.concentration} mg | {self.quantity} units | ${self.price} each"
