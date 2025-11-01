5b52bad6ac7d1fe59a2ddb1ec4f75f6d70c8ec20
EricSchwarz16 <ericschwarz2008@gmail.com>
2025-11-01 06:53:05 +0100

    Library problem implementation in cpp

---
+++ Lectie-Info-01.11.2025/proiect-tema/#4807.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+using namespace std;
+ifstream fin ("librarie.in");
+ofstream fout ("librarie.out");
+int n, A[], Q, t;
+string op;    
+int main()
+{
+    fin >> n;
+    for (int i = 1; i <= n; i++){
+        fin >> A[i];
+    }
+
+    fin >> Q;
+    while(Q--){
+        fin >> t;
+        while(t--){
+            fin >> op
+        }
+    }
+
+
+
+
+}
