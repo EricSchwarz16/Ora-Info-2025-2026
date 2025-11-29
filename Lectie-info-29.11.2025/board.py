c22f75cf6bf3edd1bd1b26efb1694415779a6401
EricSchwarz16 <ericschwarz2008@gmail.com>
2025-11-29 15:36:14 +0100

    lesson 29.11.2025

--- Lectie-info-29.11.2025/board.py
+++ Lectie-info-29.11.2025/board.py
@@ -1,10 +1,50 @@
+class OutsideBoardException(Exception):
+    def __init__(self, message):
+        self.message = message
+    
+    def __init__(self):
+        self.message = "The chosen position is outside the playing field"
+    
+    def __str__(self):
+        return self.message
+    
+
+class AlreadyPlacedSymbolException(Exception):
+    def __init__(self, message):
+        self.message = message
+    
+    def __init__(self):
+        self.message = 'The chose position already has a symbol placed'
+    
+    def __str__(self):
+        return self.message
+
 class Board:
     # 3 linii 3 coloane, la inceput o sa fie matricea plina de simbolul '.'
     def __init__(self):
-        pass
+        self.A = [['.' for col in range(3)] for row in range(3)]         # pun variabilele cu self sa le putem folosi mai departe
+        self.col = 3
+        self.row = 3
+            
     
     def __str__(self):
-        pass
+        board = ''
+        for i in range (self.col):
+            for e in self.A[i]:
+                board += e + ' '
+
+            board += '\n' 
+        
+        return board
     
-    def __place_move(row, col, symbol):
-        pass
+    def place_move(self, row, col, symbol):
+        if row >= self.row or col >= self.col or row < 0 or col < 0:
+            raise OutsideBoardException()
+
+        if self.A[row][col] != '.':
+            raise AlreadyPlacedSymbolException()
+
+
+# print(str(Board()))
+
+
