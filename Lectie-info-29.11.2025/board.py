a65b439f3a4c3606e2477f884e705304a80dd9c9
EricSchwarz16 <ericschwarz2008@gmail.com>
2025-12-06 12:22:48 +0100

    completion board

--- Lectie-info-29.11.2025/board.py
+++ Lectie-info-29.11.2025/board.py
@@ -19,10 +19,79 @@ class Board:
         return board
     
     def isFull(self):
-        pass
+        for i in range(self.row):
+            for j in range(self.col):
+                if self.A[i][j] == '.':
+                    return False
+        
+        return True
     
     def winningPlayer(self): #False in cazul in care nu am un castigator si nici egalitate, simbolul castigatorului in caz ca am un castigator sau U in cazul in care este egalitate
-        pass
+        for i in range(self.row):                   # 1) fiecare combinatie pe linie
+            ok = 1
+            for j in range(1, self.col):
+                if self.A[i][j] != self.A[i][j-1]:
+                    ok = 0
+            
+            if ok:
+                return self.A[i][0]
+        
+        for j in range(self.col):                    # 2) fiecare combinatie pe coloane
+            ok = 1
+            for i in range(1, self.row):
+                if self.A[i][j] != self.A[i-1][j]:
+                    ok = 0
+            
+            if ok:
+                return self.A[0][j]
+        
+        ok = 1
+        for i in range(1, self.row):                                         # 3.1) diagonala principala
+            if self.A[i][i] != self.A[i - 1][i - 1]:
+                ok == 0
+            
+        if ok:
+            return self.A[0][0]
+        
+        # 3.2) diagonala secundara
+
+        i, j = 1, self.col
+        ok = 1
+        while i < self.row:
+            if self.A[i][j] != self.A[i - 1][j + 1]:
+                ok = 0
+            
+            i += 1
+            j -= 1
+        
+        if ok:
+            return self.A[0][self.col]
+
+        # verificare egalitate
+
+        if self.isFull():
+            return 'U'
+
+        return False
+
+            
+
+
+             
+        
+
+            
+
+        
+        
+        
+        
+
+        
+
+        
+
+
     
     def isPlaying(self):
         if not self.isFull() and not self.winningPlayer():
