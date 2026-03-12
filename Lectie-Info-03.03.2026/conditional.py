bd452d2eadfddaad29f0fdd36e5a90164d5be502
BorodiBogdan <bogdanborodiv@gmail.com>
2026-03-12 19:54:01 +0200

    conditional variable

---
+++ Lectie-Info-03.03.2026/conditional.py
@@ -0,0 +1,69 @@
+"""
+
+readers and writers
+
+avem o resursa care poate fi citita de oricati cititori, dar care 
+uneori este schimbata de un scriitor. Putem avea mai multi cititori si mai multi
+scriitori. Vrem sa simulam operatiile facute pe aceasta resursa(citire scriere)
+
+id 1 citeste resursa -> count = 1
+id 2 citeste resursa -> count = 2
+id 3 citeste resursa -> cout = 3
+id 1 a terminat de citit -> count = 2
+id 3 a terminat de citir -> count = 1
+id 2 a terminat de citit -> count = 0
+id 4 a modificat resursa
+id 5 a modificat resursa
+id 9 citeste resursa
+id 9 termina decitit
+id 7 a modificat resursa
+
+random de cititori, numar random de scriitori
+citirea dureaza random intre 0.5 - 2s   
+
+
+is_writing
+reader_count
+condition
+
+cititor
+-----------
+while is_writing
+    condition.wait()
+
+reader_count += 1
+condition.notify_all()
+
+time.sleep(random) #simulez citirea
+
+with conditon:
+    reader_count -= 1
+    
+    if reader_count == 0
+        condition.notify_all()
+-----------
+
+
+scriitor
+-----------
+while is_writing or reader_count > 0
+    condition.wait()
+
+print("modific resursa")
+    
+condition.notidy_all() #daca cineva e in asteptare paote sa vina
+-----------
+
+
+
+with condition: #blochez lockul din conditie
+    while afara nu ploua:
+        condition.wait() -> deblochez lockul si threadul meu doarme pana cand primeste un semanl -> examplu 2 threaduri care stau aici si asteapta sa fie semnalate
+        -> daca le dau notify_all se trezesc toate
+
+meteo_checker:
+    while true:
+        daca ploua:
+            condition.notify_all() -> trezeste taote threadurile care asteapta
+            condition.notify_one()  -> trezeste un singur thread care asteapta
+"""
--- Lectie-Info-03.03.2026/tema_17.py
+++ Lectie-Info-03.03.2026/tema_17.py
@@ -1,13 +1,20 @@
 import random
 import threading
 
+
+"""
+Scrieti un program C care citeste de la tastatura un numar N si creeaza 2 thread-uri. 
+Unul dintre thread-uri va genera un numar par si il va insera intr-un sir primit ca parametru.
+Celalalt thread va genera un numar impar si il va insera in acelasi sir de numere primit ca parametru.
+Implementati un mecanism de sincronizare intre cele 2 thread-uri astfel incat alterneaza in 
+inserarea de numere in sir, pana cand sirul contine N numere.
+"""
 class SharedData:
     def __init__(self, n):
         self.n = n
         self.arr = [0] * n
         self.index = 0
-        self.lock = threading.Lock()
-        self.cond = threading.Condition(self.Lock)
+        self.cond = threading.Condition()
     
 def print_partial(prefix, arr, count):
     print(f"{prefix}: ", *arr[:count])
@@ -29,7 +36,7 @@ def thread_even(shared):
             while shared.index % 2 != 0 and shared.index < shared.n:
                 shared.cond.wait()
         
-        shared.condition.notify()
+        shared.cond.notify()
 
 def thread_odd(shared):
     with shared.cond:
@@ -48,7 +55,7 @@ def thread_odd(shared):
             while shared.index % 2 != 1 and shared.index < shared.n:
                 shared.cond.wait()
         
-        shared.condition.notify()
+        shared.cond.notify()
 
 def main():
     n = int(input("N =").strip())
