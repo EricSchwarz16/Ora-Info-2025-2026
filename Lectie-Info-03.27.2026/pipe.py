0c4dada7ed9c2d2fff2a7c1d783b2f1d85e33efc
BorodiBogdan <bogdanborodiv@gmail.com>
2026-03-27 16:07:39 +0200

    completari

--- Lectie-Info-03.27.2026/pipe.py
+++ Lectie-Info-03.27.2026/pipe.py
@@ -1,17 +1,38 @@
 from multiprocessing import Process, Pipe
+import threading
 
 #Process -> deschidem un proces nou
 #Pipe -> comunicam intre procese
 
-sum = 3
+sum = 0
+mutex = threading.Lock()
+
+def thread_child():
+    global sum
+    mutex.acquire()
+    sum += 1
+    mutex.release()
+    
 
 def child_process(conn):
-    global sum
     print(sum)
     print("Am deschis un proces")
     conn.send(["Salut", "sunt", "jmecher"])
     print("Inchid conexiunea")
     conn.close()
+    
+    nrThreads = 10
+    threads = []
+    
+    for _ in range(nrThreads):
+        th = threading.Thread(target = thread_child, args = ())
+        th.start()
+        threads.append(th)
+        
+    for t in threads:
+        t.join()
+        
+    print(f"Suma este {sum}")
 
 if __name__ == "__main__":
     parent_conn, child_conn = Pipe() #capetele pipe-ului
