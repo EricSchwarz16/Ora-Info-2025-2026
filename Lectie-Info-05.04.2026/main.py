1a10bde5f362cd7a9a3070de80f879650a943a00
EricSchwarz16 <ericschwarz2008@gmail.com>
2026-04-05 14:10:11 +0200

    changes

--- Lectie-Info-05.04.2026/main.py
+++ Lectie-Info-05.04.2026/main.py
@@ -1,6 +1,9 @@
 from news_repository import NewsRepository
 import datetime
-
+ 
 repo = NewsRepository()
 
+repo.increment_views(2, date=datetime.datetime.now())
+repo.increment_views(2, date=datetime.datetime.now())
+repo.increment_views(2, date=datetime.datetime.now())
 print(repo.get_top_k(2, 1))
