ba276d23902e82fb48968bab55391ddb65c01767
BorodiBogdan <bogdanborodiv@gmail.com>
2026-04-19 14:08:31 +0300

    organizare

---
+++ Lectie-Info-05.04.2026/tema/main.py
@@ -0,0 +1,11 @@
+from repositories.news_repository import NewsRepository
+import datetime
+ 
+repo = NewsRepository()
+
+repo.increment_views(2, date=datetime.datetime.now())
+repo.increment_views(2, date=datetime.datetime.now())
+repo.increment_views(2, date=datetime.datetime.now())
+
+print(repo.get_top_k(2, 1))
+print(repo.get_top_k(10, 30))
